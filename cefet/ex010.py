04ae78d34f797d2bfcc74be6af69e31c972c210a
Pedro <pedrobznss@gmail.com>
2026-08-11 09:27:19 -0300

    Create ex010.py

---
+++ cefet/ex010.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia um conjunto de linhas, contendo, cada 
+# uma, o número de um empregado, a hora de início (hora, minuto e 
+# segundo) e a hora do término (hora, minuto e segundo) de uma 
+# determinada tarefa. A última linha conterá o número do empregado 
+# igual a zero. Calcule para cada empregado, a duração da tarefa que ele 
+# executou. Escreva para cada empregado, o seu número e a duração da 
+# sua tarefa em horas, minutos e segundos.
+# Obs.: Usar uma função que receba como parâmetros horas, minutos e 
+# segundos e retorne o total de segundos. Usar uma função que receba 
+# como parâmetro o total de segundos e retorne em horas, minutos e 
+# segundos.
+
